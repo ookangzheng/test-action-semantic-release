7529194f000e6d7439a71b1dd9aaeba3e6877945
ookangzheng <kangzheng.oo@coolbitx.com>
2021-01-13 18:40:36 +0800

    fix: child process

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 // const sh = require('shelljs');
 const { spawn } = require('child_process');
 
