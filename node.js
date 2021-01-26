545101da1ec3acd6409d8a9b592f1cb5ecad4c0e
ookangzheng <kangzheng.oo@coolbitx.com>
2021-01-26 17:52:46 +0800

    fix: SYG-456 typo

--- node.js
+++ node.js
@@ -1,8 +1,7 @@
 const child = require('child_process');
 const fs = require('fs');
 
-const output = child.execSync(`git log --format=%B%H----DELIMITER----`).toString('utf-8');
-
+const output = child.execSync(`git log --format=%B%H----DELIMITER----`).toString('utf-8')
 const commitsArray = output.split('----DELIMITER----\n').map(commit => {
   const [message, sha] = commit.split('\n');
   return { sha, message };
