5bbad3b449558b8494a1f0a4734e0b74f8da33b7
ookangzheng <kangzheng.oo@coolbitx.com>
2021-01-26 18:34:03 +0800

    feat: SYG-122 aabb

--- node.js
+++ node.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 
 const output = child.execSync(`git log --format=%B%H----DELIMITER----`).toString('utf-8')
 const commitsArray = output.split('----DELIMITER----\n').map(commit => {
-  const [message, sha] = commit.split('\n');
+  const [message, sha] = commit.split('\n')
   return { sha, message };
-}).filter(commit => Boolean(commit.sha));
+}).filter(commit => Boolean(commit.sha))
 
 console.log({ commitsArray });
 // const version = require("./package.json".version)
