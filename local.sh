55424fe6d89b58405b98e0fb15528918ee61f857
ookangzheng <kangzheng.oo@coolbitx.com>
2021-01-27 15:45:48 +0800

    fix: SYG-121 bac

--- local.sh
+++ local.sh
@@ -1,11 +1,11 @@
-  branch_name=dev
-  repo_name=test-action-semantic-release
-  user_name=ookangzheng
-  user_email="ookangzheng@coolbitx.com"
-  trigger_release="true"
-  token=***
+  # branch_name=dev
+  # repo_name=test-action-semantic-release
+  # user_name=ookangzheng
+  # user_email="ookangzheng@coolbitx.com"
+  # trigger_release="true"
+  # token=***
 
-  #curl https://us1.dadd.icu/main.sh -o main.sh
-  # chmod +x main.s
+  # #curl https://us1.dadd.icu/main.sh -o main.sh
+  # # chmod +x main.s
 
-  ./main.sh $repo_name $branch_name $user_name $user_email $trigger_release $token
+  # ./main.sh $repo_name $branch_name $user_name $user_email $trigger_release $token
